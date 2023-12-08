21694b2e372a7a6c86a9176e360e29b113677229
emlala <emmi.lahtisalo@gmail.com>
2023-12-08 22:40:30 +0200

    advent of code 2023 day 6 part 2

--- day6.py
+++ day6.py
@@ -9,13 +9,16 @@ file.close()
 test_data = ["Time:      7  15   30",
              "Distance:  9  40  200"]
 
-def tidy_lines(lines):
+# part 1
+def tidy_lines_1(lines):
     lines = [line.strip().split() for line in lines]
     [line.pop(0) for line in lines]
     lines = [[int(l) for l in line] for line in lines]
     pairs = np.column_stack(lines)
 
-def beat_the_record(pair):
+    return pairs
+
+def beat_the_record_1(pair):
     time = pair[0]
     record = pair[1]
     number_of_ways = 0
@@ -30,11 +33,45 @@ def beat_the_record(pair):
 
     return number_of_ways
 
-games = tidy_lines(lines)
-answer = 1
-for game in games:
-    ways = beat_the_record(game)
-    answer *= ways
-    print("number of ways:", ways)
+def get_answer_1(lines):
+    games = tidy_lines_1(lines)
+    answer = 1
+    
+    for game in games:
+        ways = beat_the_record_1(game)
+        answer *= ways
+        print("number of ways:", ways)
 
-print("answer:", answer)
+    return answer
+
+# part 2
+def tidy_lines_2(lines):
+    lines = [line.strip() for line in lines]
+    lines = [line[line.find(": ") + 1:] for line in lines]
+    lines = [line.replace(" ", "") for line in lines]
+    lines = [int(line) for line in lines]
+    return lines
+
+def beat_the_record_2(pair):
+    time = pair[0]
+    record = pair[1]
+    number_of_ways = 0
+    button = 0
+
+    while button <= time:
+        time_left = time - button
+        distance = button * time_left
+        if distance > record:
+            number_of_ways += 1
+        button += 1
+
+    return number_of_ways
+
+def get_answer_2(lines):
+    game = tidy_lines_2(lines)
+    ways = beat_the_record_2(game)
+    return ways
+
+
+#print("answer 1:", get_answer_1(lines))
+print("answer 2:", get_answer_2(lines))
